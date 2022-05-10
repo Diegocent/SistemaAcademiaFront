33039b4004eb3be62011d88abd7f79f540ff623f
Alex Gerson <alex.gerson@hotmail.com>
2022-05-10 12:32:28 -0400

    Eliminar alumno

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
@@ -20,6 +21,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     RouterModule,
     AppRoutingModule,
     MatTableModule,
+    MatButtonModule
   ],
   declarations: [
     AppComponent,
--- src/app/modificar_alumna/table-list.component.ts
+++ src/app/modificar_alumna/table-list.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
 import { Alumno, Persona, PersonaAlumno } from 'app/models/models';
 import { AlumnoService } from 'app/service/alumno.service';
 import { PersonaService } from 'app/service/persona.service';
+
 @Component({
   selector: 'app-table-list',
   templateUrl: './table-list.component.html',
@@ -12,6 +15,10 @@ export class TableListComponent implements OnInit {
   personas: Persona[] = [];
   alumnos: Alumno[] = [];
   personaAlumno: PersonaAlumno[] = [];
+  pageSlice = this.personaAlumno.slice(0, 3);
+  
+  dataSource: PersonaAlumno[] = []
+  displayedColumns: string[] = ['cedula', 'nombre', 'vestuario', 'entrada', 'cantidad_materias', 'derecho_examen', 'accion'];
 
   constructor(private personaService: PersonaService, private alumnoService: AlumnoService) { }
 
@@ -28,8 +35,41 @@ export class TableListComponent implements OnInit {
             alumno: this.alumnos[i]
           }
           this.personaAlumno = [...this.personaAlumno, temp]
+          this.dataSource = [...this.dataSource, temp]
         }
       })
     })
+  }
+
+  eliminarAlumno(data, index) {
+    console.log(data, index)
+    Confirm.show(
+      'Eliminar alumno/a',
+      '¿Está seguro?',
+      'Si',
+      'No',
+      () => {
+        this.personaService.delete(data.persona.id)
+          .subscribe(response => {
+            this.alumnoService.delete(data.alumno.id).subscribe(res => {
+              console.log(response);
+              let q = [];
+              this.dataSource.forEach((e, i) => {
+                if(i != index)
+                  q = [...q, e]
+              })
+              this.dataSource = q;
+              Report.success(
+                'Éxito',
+                'Se eliminó al alumno/a exitosamente',
+                'Ok',
+                );
+            })
+          })
+      },
+      () => {},
+      {
+      },
+      );
   }
 }
